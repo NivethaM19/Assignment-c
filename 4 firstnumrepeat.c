17301df162a288945c2b6dff81c191cdb35966d5
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2018-01-17 22:03:07 +0530

    Update 4 firstnumrepeat.c

--- 4 firstnumrepeat.c
+++ 4 firstnumrepeat.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
 int main(void) {
-	int a[10];
-	int i;
-	for(i=0;i<10;i++)
+	int a[5];
+	int k;
+	for(k=0;k<5;k++)
 	{
-		scanf("%d",&a[i]);
+		scanf("%d",&a[k]);
 	}
-	for(i=0;i<10;i++)
+	int i,j,flag=0;
+	for(i=0;i<5;i++)
 	{
-		if(a[i]==a[i+1])
+		for(j=i+1;j<5;j++)
 		{
-			printf("%d",a[i]);
+			if(a[i]==a[j])
+			{
+				printf("%d",a[i]);
+				flag=1;
+				break;
+			}
+			
+		}
+		if(flag==1)
+		{
 			break;
 		}
 	}
