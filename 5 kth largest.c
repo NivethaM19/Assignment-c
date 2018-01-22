b2b5827a24a8a9a343371dc2f0879d3b1b5954fc
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2018-01-22 20:11:43 +0530

    Create 5 kth largest.c

---
+++ 5 kth largest.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,k;
+	scanf("%d %d",&n,&k);
+	int a[n];
+	int i,j;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				int temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	printf("%d",a[k-1]);
+	return 0;
+}
